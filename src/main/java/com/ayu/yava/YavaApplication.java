493f8fb3678c02bf7ef2d180b88ba1bfc6eebe7b
aYu <ayuisokay@qq.com>
2025-03-17 21:30:31 +0800

    init: 初始化项目

---
+++ src/main/java/com/ayu/yava/YavaApplication.java
@@ -0,0 +1,13 @@
+package com.ayu.yava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayu/yava/YavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayu.yava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
